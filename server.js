dfd1e47088a8f548a818271bd1f22a752568aa11
Sam Kaupa <skaupa@EDARANU.local>
2021-01-25 11:15:12 +1000

    Final Submissions!

--- server.js
+++ server.js
@@ -2,19 +2,31 @@ let express = require('express');
 let path = require('path');
 let app = express();
 
-app.use('/style.css',express.static(path.join(__dirname + '/public')));
+app.use(express.static('public'));
+
+
+app.get('/welcome', function(req,res){
+    res.sendfile(path.join(__dirname,'welcome.html'))
+});
+
 
 app.get('/home', function(req,res){
     res.sendfile(path.join(__dirname,'adventure.html'))
-})
+});
 
 app.get('/buy', function(req,res){
     res.sendfile(path.join(__dirname,'buy.html'))
-})
+});
 
 app.get('/build', function(req,res){
     res.sendfile(path.join(__dirname,'build.html'))
-})
+});
+app.get('/discipline', function(req,res){
+    res.sendfile(path.join(__dirname,'discipline.html'))
+});
+app.get('/food', function(req,res){
+    res.sendfile(path.join(__dirname,'food.html'))
+});
  
 
 app.listen(8080)
